b28294fd27bf002b36f0679d624167d209f639fb
risca <me@risca.eu>
2022-11-16 16:25:02 +0100

    support latest debian testing
    	modified:   pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -10,5 +10,7 @@ if [[ ! -f /etc/debian_version ]]; then
 fi
 
 apt-get update
-apt-get install -y debootstrap debian-archive-keyring jq dpkg-dev gnupg apt-transport-https ca-certificates curl gpg
+apt-get install -y debian-archive-keyring jq dpkg-dev gnupg apt-transport-https ca-certificates curl gpg wget
+wget http://ftp.us.debian.org/debian/pool/main/d/debootstrap/debootstrap_1.0.128+nmu2_all.deb
+apt install -y ./debootstrap*
 
