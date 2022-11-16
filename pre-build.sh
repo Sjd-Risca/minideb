81a58df9fd5dc34bb88d8cc361d26a96ced6c505
risca <me@risca.eu>
2022-11-16 16:27:40 +0100

    latest

--- pre-build.sh
+++ pre-build.sh
@@ -12,5 +12,5 @@ fi
 apt-get update
 apt-get install -y debian-archive-keyring jq dpkg-dev gnupg apt-transport-https ca-certificates curl gpg wget
 wget http://ftp.us.debian.org/debian/pool/main/d/debootstrap/debootstrap_1.0.128+nmu2_all.deb
-apt install -y ./debootstrap*
+apt install -y ./debootstrap_1.0.128+nmu2_all.deb
 
